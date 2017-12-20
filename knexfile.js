cb3f647ffb7790a87fd7eb3e7345404fe9602284
AndyY <yan.andy4@gmail.com>
2017-12-19 19:18:49 -0700

    first commit

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+const databaseName = 'passport_local_knex';
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: `postgres://localhost:5432/${databaseName}`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    },
+    debug: true
+  },
+  test: {
+    client: 'postgresql',
+    connection: `postgres://localhost:5432/${databaseName}_test`,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
+  }
+};
---
+++ test/integration/routes.auth.test.js
@@ -0,0 +1,42 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const server = require('../../src/server/app');
+const knex = require('../../src/server/db/connection');
+
+describe('routes : auth', () => {
+
+  beforeEach(() => {
+    return knex.migrate.rollback()
+    .then(() => { return knex.migrate.latest(); })
+    .then(() => { return knex.seed.run(); });
+  });
+
+  afterEach(() => {
+    return knex.migrate.rollback();
+  });
+
+  describe('POST /auth/register', () => {
+    it('should register a new user', (done) => {
+      chai.request(server)
+      .post('/auth/register')
+      .send({
+        username: 'michael',
+        password: 'herman'
+      })
+      .end((err, res) => {
+        should.not.exist(err);
+        res.redirects.length.should.eql(0);
+        res.status.should.eql(200);
+        res.type.should.eql('application/json');
+        res.body.status.should.eql('success');
+        done();
+      });
+    });
+  });
+
+});
