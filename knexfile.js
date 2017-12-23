a226c1a734f70babcb4f7dfcbc7e2315a799b62c
AndyY <yan.andy4@gmail.com>
2017-12-23 16:18:41 -0700

    Update knexfile

--- knexfile.js
+++ knexfile.js
@@ -21,5 +21,15 @@ module.exports = {
     seeds: {
       directory: __dirname + '/src/server/db/seeds'
     }
-  }
+  },
+  production: {
+    client: 'postgresql',
+    connection: 'postgres://fgjesboiglenwu:f300fca75d24368fdd3555c1e264983c9036324a96d43d62b3766b8b1a419df4@ec2-107-21-201-57.compute-1.amazonaws.com:5432/d7rkb9kurq4tra',
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    },
+    ssl: true
 };
